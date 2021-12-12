bef5175354970dc2f8d384df104649a0d94de629
Kevin Carroll <kevin.carroll@outlook.com>
2021-12-12 14:49:14 -0700

    Updated lib version and added firebase demo link (#7)

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -31,7 +31,7 @@ class HomeSplash extends React.Component {
             <h2 className="projectTitle">
                 {<span>GraphQL ASP.NET</span>}
                 {/*<small>{siteConfig.tagline}</small>*/}
-                <small>v0.8.1-beta</small>
+                <small>v0.9.1-beta</small>
             </h2>
         );
 
